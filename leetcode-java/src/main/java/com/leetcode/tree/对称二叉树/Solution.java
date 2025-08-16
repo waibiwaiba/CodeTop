89431a2af5c543e020d827586c88e24b22dc5a2f
matrix <wabby.wabbo.250@gmail.com>
2025-08-16 21:59:23 +0800

    add better code in comment

--- leetcode-java/src/main/java/com/leetcode/tree/对称二叉树/Solution.java
+++ leetcode-java/src/main/java/com/leetcode/tree/对称二叉树/Solution.java
@@ -33,3 +33,20 @@ class Solution {
         return isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
     }
 }
+
+// BETTER
+// private boolean isMirror(TreeNode node1, TreeNode node2) {
+//     // 如果两个节点都为空，那么它们是镜像对称的
+//     if (node1 == null && node2 == null) {
+//         return true;
+//     }
+
+//     // 如果只有一个节点为空，或者两个节点的值不相等，
+//     // 那么它们肯定不是镜像对称的
+//     if (node1 == null || node2 == null || node1.val != node2.val) {
+//         return false;
+//     }
+
+//     // 递归地“交叉”检查它们的子树是否也互为镜像
+//     return isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
+// }
